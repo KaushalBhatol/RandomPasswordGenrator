46910784b8b7d7c0b13aa7695700c5b9c15a678c
KaushalBhatol <kaushal.bhatol@outlook.com>
2022-11-23 10:54:01 +0530

    version 1 (cli)

--- main.py
+++ main.py
@@ -1,17 +1,39 @@
-#!/bin/python3
-from random import choice   # using random module for choice function
-import library.user_intrect as ui   # importing user_intrect module as ui
+#!/usr/bin/python3
 
-passwd = ui.prefix()
-length = ui.len_of_passwd()-len(passwd)
+# @purp: genrating random pass
+# @auth: Kaushal Chaudhry
+#!/usr/bin/python3
+from logic import main
 
-if len(passwd) > length or length > 60:
-    print("Please enter valid length")
-else:
-    choice_of_char = ui.choice_of_char()
-    # loop for appending random character in passwd string
-    for i in range(length):
-        random_char = choice(choice(choice_of_char))
-        passwd += random_char
+# prefix, length_passwd, small_char, capital_char, number_char, special_char
 
-    print("Your password is:", passwd)
+
+def user_input():
+    print("\n\t Password Genrator \n")
+    prefix = input("enter prefix:\t")
+    length_passwd = int(input("enter length:\t"))
+    if input("do you want small char? [Y/n]:  ") == "n":
+        small_char = False
+    else:
+        small_char = True
+
+    if input("do you want capital char? [Y/n]: ") == "n":
+        capital_char = False
+    else:
+        capital_char = True
+
+    if input("do you want number char? [Y/n]:  ") == "n":
+        number_char = False
+    else:
+        number_char = True
+
+    if input("do you want special char? [Y/n]: ") == "n":
+        special_char = False
+    else:
+        special_char = True
+
+    return (main(prefix, length_passwd, small_char,
+                 capital_char, number_char, special_char))
+
+
+print("\nGenrated passwd: \t", user_input(), "\n")
